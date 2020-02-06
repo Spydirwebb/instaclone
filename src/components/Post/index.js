4f4ccb1ac00bdc8cdd40cf11d64e5cdd21776ec7
Jeff <webb.jeffery.89@gmail.com>
2020-02-05 23:57:33 -0800

    Initial commit. Static working

---
+++ src/components/Post/index.js
@@ -0,0 +1,48 @@
+// src/components/Post/index.js
+
+//imports
+import React, { Component } from 'react';
+import "./Post.css";
+
+/*post class
+header = avatar picture, nickname
+image = image
+caption = caption
+*/
+class Post extends Component {
+  constructor(props){
+    super(props);
+  }
+  render () {
+    const nickname = this.props.nickname;
+    const avatar = this.props.avatar;
+    const image = this.props.image;
+    const caption = this.props.caption;
+
+    return (
+      <article className="Post" ref="Post">
+        <header>
+          <div className="Post-user">
+            <div className="Post-user-avatar">
+              <img src={avatar} alt={nickname} />
+            </div>
+            <div className="Post-user-nickname">
+              <span>{nickname}</span>
+            </div>
+          </div>
+        </header>
+        <div className="Post-image">
+          <div className="Post-image-bg">
+            <img alt={caption} src={image} />
+          </div>
+        </div>
+        <div className="Post-caption">
+          <strong>{nickname}</strong> {caption}
+        </div>
+      </article>
+    );
+  }
+}
+
+//export
+export default Post;
